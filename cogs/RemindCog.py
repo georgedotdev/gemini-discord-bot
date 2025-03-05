14c4797c9d56a764cd6781e09360e8944a6776a9
georgedotdev <georgemathew.com@gmail.com>
2025-03-05 12:04:20 +0530

    fixed the reminders, made it so you can add reminder at a specific date and time. remind is working but reminder not working. added music bot, no errors, but music is not playing and queue seems to be not working. will debug and imporve that next version

--- cogs/RemindCog.py
+++ cogs/RemindCog.py
@@ -21,13 +21,13 @@ class RemindAgent(commands.Cog):
         self.check_reminders.cancel()
 
     @commands.command()
-    async def remind(self, ctx, *, reminder_text):
-        """Set a reminder. Format: !remind YYYY-MM-DD HH:MM Your reminder message"""
+    async def reminder(self, ctx, *, reminder_text):
+        """Set a reminder with a specific date and time. Format: !reminder YYYY-MM-DD HH:MM Your reminder message"""
         pattern = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}) (.+)"
         match = re.match(pattern, reminder_text)
         
         if not match:
-            await ctx.send("Invalid format. Use: !remind YYYY-MM-DD HH:MM Your reminder message")
+            await ctx.send("Invalid format. Use: !reminder YYYY-MM-DD HH:MM Your reminder message")
             return
 
         time_str, message = match.groups()
@@ -44,6 +44,35 @@ class RemindAgent(commands.Cog):
         self.reminders.append(reminder)
         await ctx.send(f"Reminder set for {time_str}: {message}")
 
+    @commands.command()
+    async def remind(self,ctx,time,*,task):
+        def convert(time):
+            pos = ['s','m','h','d']
+            time_dict = {'s': 1, 'm': 60, 'h': 3600, 'd': 3600*24}
+            unit = time[-1]
+
+            if unit not in pos:
+                return -1
+            try:
+                val = int(time[:-1])
+                return val*time_dict[unit]
+            except:
+                return -2
+        converted_time = convert(time)
+
+        if converted_time == -1:
+            await ctx.send("You didn't provide the proper unit")
+            return
+        elif converted_time == -2:
+            await ctx.send("The time must be an integer")
+            return
+        await ctx.send(f"I will remind you about {task} in {time}")
+
+        await asyncio.sleep(converted_time)
+        await ctx.send(f"{ctx.author.mention} Your reminder for {task} is up!")
+
+
+        
     @commands.command()
     async def list_reminders(self, ctx):
         """List all reminders for the user"""
